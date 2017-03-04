838a12992b7bbf37b7ca1e083bdfccf800379b3e
BlackHat <PASwd4@users.noreply.github.com>
2017-03-03 22:47:10 -0600

    Add files via upload

---
+++ hatclient/js/init.js
@@ -0,0 +1,11 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+	$('.modal').modal();
+    $('select').material_select();
+	$('writemsg').modal();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
